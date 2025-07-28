1df3015dcb615488077487b68dbd60a2e4d1bb49
Oliver <oliverng826@gmail.com>
2025-07-28 14:07:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function revealGift() {
   const input = document.getElementById("nameInput").value.trim().toLowerCase();
-  if (input === "monica" || input === "herfullname") {
+  if (input === "subul" || input === "herfullname") {
     document.getElementById("gift").style.display = "block";
   } else {
-    alert("Try again 😏");
+    alert("Be honest");
   }
 }
