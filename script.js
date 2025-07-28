fd7e3ce8f99cdec5796102a30211b97e0e5b8dae
Oliver <oliverng826@gmail.com>
2025-07-28 14:16:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function revealGift() {
   const input = document.getElementById("nameInput").value.trim().toLowerCase();
-  if (input === "subul" || input === "herfullname") {
+  if (input === "herfullname") {
     document.getElementById("gift").style.display = "block";
   } else {
     alert("Be honest");
